8e137448f1e357e04df113dbcbbb0ebdcc87d660
jabbawalden <33333251+jabbawalden@users.noreply.github.com>
2018-05-14 18:58:40 +1000

    Code modification
    restructure player code + tripple shot behaviour

--- Assets/C#/PShoot.cs
+++ Assets/C#/PShoot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PShoot : MonoBehaviour {
 
+    
+
     [SerializeField]
     private float _laserSpeed;
 
@@ -13,6 +15,11 @@ public class PShoot : MonoBehaviour {
     [SerializeField]
     private float _fireRate;
     private float _nextFire;
+
+    public bool trippleShotUpgrade = false;
+    private float _trippleTime = 0.0f;
+    [SerializeField]
+    private float _trippleSetTime;
 
     // Use this for initialization
     private void Start ()
@@ -24,10 +31,41 @@ public class PShoot : MonoBehaviour {
 	// Update is called once per frame
 	private void Update ()
     {
-        PlayerShoot();
-	}
+        PlayerShootInput();
+        TrippleShotUpgrade();
 
-    private void PlayerShoot ()
+    }
+
+    private void TrippleShotUpgrade ()
+    {
+        if (trippleShotUpgrade)
+        {
+            _trippleSetTime -= 1;
+            
+            if (_trippleSetTime <= 0)
+            {
+                trippleShotUpgrade = false;
+            }
+        }
+
+        if (!trippleShotUpgrade)
+        {
+            _trippleSetTime = 500;
+            
+        }
+
+
+    }
+
+    private void PlayerShootInput ()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot ()
     {
         //if player presses space, instantiate laser at position above player
         //have lazer travelling at set speed
@@ -35,15 +73,35 @@ public class PShoot : MonoBehaviour {
         //before allowing for firing again.
 
         Vector3 spawnPos = transform.position + new Vector3(0, 0.88f, 0);
-        
-        if (Input.GetKey(KeyCode.Space) && Time.time > _nextFire || Input.GetMouseButton(0) && Time.time > _nextFire)
+        Vector3 spawnPos2 = transform.position + new Vector3(0.5f, -0.2f, 0);
+        Vector3 spawnPos3 = transform.position + new Vector3(-0.5f, -0.2f, 0);
+
+        if (Time.time > _nextFire)
         {
             //spawn laserCopy
             _nextFire = Time.time + _fireRate;
 
-            GameObject lFinal = Instantiate(_laserSpawn, spawnPos, Quaternion.identity);
-            lFinal.GetComponent<Rigidbody2D>().velocity = new Vector3(0, _laserSpeed, 0);
+            GameObject Shot1 = Instantiate(_laserSpawn, spawnPos, Quaternion.identity);
+            Shot1.GetComponent<Rigidbody2D>().velocity = new Vector3(0, _laserSpeed, 0);
+
+            if (trippleShotUpgrade)
+            {
+                GameObject Shot2 = Instantiate(_laserSpawn, spawnPos2, Quaternion.identity);
+                Shot2.GetComponent<Rigidbody2D>().velocity = new Vector3(0, _laserSpeed, 0);
+
+                GameObject Shot3 = Instantiate(_laserSpawn, spawnPos3, Quaternion.identity);
+                Shot3.GetComponent<Rigidbody2D>().velocity = new Vector3(0, _laserSpeed, 0);
+
+                
+            }
+           
         }
       
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
 }
